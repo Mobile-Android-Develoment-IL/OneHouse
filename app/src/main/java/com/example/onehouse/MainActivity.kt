a1dd1c6fd925185deacd53793ae9c93f94861dff
bagasadhi495 <bagasadhi495@gmail.com>
2024-06-13 00:37:42 +0700

    SplasScreen,Register,SignIn

--- app/src/main/java/com/example/onehouse/MainActivity.kt
+++ app/src/main/java/com/example/onehouse/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.onehouse.Routing.Navbar
 import com.example.onehouse.ui.theme.OneHouseTheme
 
@@ -12,10 +19,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            OneHouseTheme {
-                Navbar()
+            val navController = rememberNavController()
+
+            OneHouseTheme{
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    NavHost(navController = navController, startDestination = "sigInScreen") {
+
+                        composable("signInScreen") {
+                            SignIn(navController = navController)
+                        }
+                        composable("register") {
+                            Register(navController = navController)
+                        }
+                        composable("home") {
+                            Navbar()
+                        }
+                    }
+
                 }
             }
         }
     }
+}
 
--- app/src/main/java/com/example/onehouse/Register.kt
+++ app/src/main/java/com/example/onehouse/Register.kt
@@ -1,2 +1,103 @@
 package com.example.onehouse
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun Register(navController: NavController) {
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(text = "Masuk Sekarang", fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Image(painter = painterResource(id = R.drawable.logo_onehouse),
+            contentDescription = "Login Image",
+            modifier = Modifier.size(250.dp))
+
+        Text(text = "Halo selamat datang!!", fontSize = 15.sp, fontWeight = FontWeight.Medium)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(text = "Mari mulai membuat akun Anda", fontSize = 15.sp, fontWeight = FontWeight.Medium)
+
+        Spacer(modifier = Modifier.height(9.dp))
+        OutlinedTextField(value = email, onValueChange = {email = it}, label = {
+            Text(text = "Nama Lengkap")
+        })
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(value = email, onValueChange = {email = it}, label = {
+            Text(text = "Email/No. Hp")
+        })
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(value = password, onValueChange = {password = it}, label = {
+            Text(text = "Kata Sandi")
+        }, visualTransformation = PasswordVisualTransformation())
+
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier.padding(top = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Belum punya akun?",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light
+            )
+            Text(
+                text = "Masuk",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.clickable {
+                    navController.navigate("signInScreen")
+                }
+            )
+        }
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(0.5f),
+            onClick = {
+//            Log.i("Credential", "Email : $email Password : $password")
+                navController.navigate("signInScreen")
+            }) {
+            Text(text = "Daftar")
+        }
+    }
+}
